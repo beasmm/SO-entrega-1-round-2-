a29b5ea415360e7e47a2b28a64af9ab70bf925bb
catalms <catilms13@gmail.com>
2023-12-03 14:51:31 +0000

    exercise 1 done

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdbool.h>
 
 //can this b here
 
@@ -15,7 +19,6 @@
 
 int main(int argc, char *argv[]) {
   unsigned int state_access_delay_ms = STATE_ACCESS_DELAY_MS;
-  int fp = open("./jobs/test.jobs", O_RDONLY);
 
   if (argc > 1) {
     char *endptr;
@@ -34,7 +37,36 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  while (1) {
+  char* dir_path = "jobs";
+  DIR *dir = opendir(dir_path);
+  if (dir == NULL) {
+    fprintf(stderr, "Failed to open jobs directory\n");
+    return 1;
+  }
+
+  struct dirent* ent;
+  int fp;
+
+  bool not_done = true;
+  while (not_done) {
+    ent = readdir(dir);
+    char file_path[128];
+    memset(file_path, 0, 128);
+
+    if (ent == NULL) break;
+    if (ent->d_name[0] == '.') continue;
+
+    strcat(file_path, dir_path);
+    strcat(file_path, "/");
+    strcat(file_path, ent->d_name);
+
+    fp = open(file_path, O_RDONLY);
+    if (fp == -1) {
+      not_done = false;
+      fprintf(stderr, "Failed to open jobs file\n");
+      continue;
+    }
+
     unsigned int event_id, delay;
     size_t num_rows, num_columns, num_coords;
     size_t xs[MAX_RESERVATION_SIZE], ys[MAX_RESERVATION_SIZE];
@@ -42,89 +74,94 @@ int main(int argc, char *argv[]) {
     printf("> ");
     fflush(stdout);
 
-    switch (get_next(fp)) {
-      case CMD_CREATE:
-        if (parse_create(fp, &event_id, &num_rows, &num_columns) != 0) {
+    bool eof = true;
+    while (eof){
+      switch (get_next(fp)) {
+        case CMD_CREATE:
+          if (parse_create(fp, &event_id, &num_rows, &num_columns) != 0) {
+            fprintf(stderr, "Invalid command. See HELP for usage\n");
+            continue;
+          }
+
+          if (ems_create(event_id, num_rows, num_columns)) {
+            fprintf(stderr, "Failed to create event\n");
+          }
+
+          break;
+
+        case CMD_RESERVE:
+          num_coords = parse_reserve(fp, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+
+          if (num_coords == 0) {
+            fprintf(stderr, "Invalid command. See HELP for usage\n");
+            continue;
+          }
+
+          if (ems_reserve(event_id, num_coords, xs, ys)) {
+            fprintf(stderr, "Failed to reserve seats\n");
+          }
+
+          break;
+
+        case CMD_SHOW:
+          if (parse_show(fp, &event_id) != 0) {
+            fprintf(stderr, "Invalid command. See HELP for usage\n");
+            continue;
+          }
+
+          if (ems_show(event_id)) {
+            fprintf(stderr, "Failed to show event\n");
+          }
+
+          break;
+
+        case CMD_LIST_EVENTS:
+          if (ems_list_events()) {
+            fprintf(stderr, "Failed to list events\n");
+          }
+
+          break;
+
+        case CMD_WAIT:
+          if (parse_wait(fp, &delay, NULL) == -1) {  // thread_id is not implemented
+            fprintf(stderr, "Invalid command. See HELP for usage\n");
+            continue;
+          }
+
+          if (delay > 0) {
+            printf("Waiting...\n");
+            ems_wait(delay);
+          }
+
+          break;
+
+        case CMD_INVALID:
           fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
-        }
+          break;
 
-        if (ems_create(event_id, num_rows, num_columns)) {
-          fprintf(stderr, "Failed to create event\n");
-        }
+        case CMD_HELP:
+          printf(
+              "Available commands:\n"
+              "  CREATE <event_id> <num_rows> <num_columns>\n"
+              "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
+              "  SHOW <event_id>\n"
+              "  LIST\n"
+              "  WAIT <delay_ms> [thread_id]\n"  // thread_id is not implemented
+              "  BARRIER\n"                      // Not implemented
+              "  HELP\n");
 
-        break;
+          break;
 
-      case CMD_RESERVE:
-        num_coords = parse_reserve(fp, MAX_RESERVATION_SIZE, &event_id, xs, ys);
+        case CMD_BARRIER:  // Not implemented
+        case CMD_EMPTY:
+          break;
 
-        if (num_coords == 0) {
-          fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
-        }
-
-        if (ems_reserve(event_id, num_coords, xs, ys)) {
-          fprintf(stderr, "Failed to reserve seats\n");
-        }
-
-        break;
-
-      case CMD_SHOW:
-        if (parse_show(fp, &event_id) != 0) {
-          fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
-        }
-
-        if (ems_show(event_id)) {
-          fprintf(stderr, "Failed to show event\n");
-        }
-
-        break;
-
-      case CMD_LIST_EVENTS:
-        if (ems_list_events()) {
-          fprintf(stderr, "Failed to list events\n");
-        }
-
-        break;
-
-      case CMD_WAIT:
-        if (parse_wait(fp, &delay, NULL) == -1) {  // thread_id is not implemented
-          fprintf(stderr, "Invalid command. See HELP for usage\n");
-          continue;
-        }
-
-        if (delay > 0) {
-          printf("Waiting...\n");
-          ems_wait(delay);
-        }
-
-        break;
-
-      case CMD_INVALID:
-        fprintf(stderr, "Invalid command. See HELP for usage\n");
-        break;
-
-      case CMD_HELP:
-        printf(
-            "Available commands:\n"
-            "  CREATE <event_id> <num_rows> <num_columns>\n"
-            "  RESERVE <event_id> [(<x1>,<y1>) (<x2>,<y2>) ...]\n"
-            "  SHOW <event_id>\n"
-            "  LIST\n"
-            "  WAIT <delay_ms> [thread_id]\n"  // thread_id is not implemented
-            "  BARRIER\n"                      // Not implemented
-            "  HELP\n");
-
-        break;
-
-      case CMD_BARRIER:  // Not implemented
-      case CMD_EMPTY:
-        break;
-
-      case EOC:
-        ems_terminate();
-        return 0;
+        case EOC:
+          eof = false;
+          break;
+      }
     }
   }
+  ems_terminate();
+  closedir(dir);
 }
